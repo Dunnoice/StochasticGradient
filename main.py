8727ac7c6c99d8403bf9b2d8df53969325f98c2b
Dunnoice <22249898+Dunnoice@users.noreply.github.com>
2019-12-03 15:31:32 +1000

    Improvements & bugfixes
    Bug: weights change too drastically -- overflow at iteration < 100

--- main.py
+++ main.py
@@ -74,26 +74,29 @@ class Precedent(collections.namedtuple('Precedent', 'x y')):
 class Sample(tuple):
 	"""
 	Formatted sample
-
-	x starts from 1 in order to multiply to weights
-
-	x[0] = artificial constant attribute
 	"""
 
-	def __new__(cls, value):
+	def __new__(cls, value, add_const_attr=False):
 		"""
 		:type value: list[list[float]]
+		:param add_const_attr: x[0] = artificial constant attribute (default=1),
+		x starts from 1 in order to multiply to weights
 		"""
 		values = []
 		for sample_i in value:
 			i_y = len(sample_i) - 1
-			values.append(Precedent([1.] + sample_i[0:i_y], sample_i[i_y]))
+			x_i = sample_i[0:i_y]
+			if add_const_attr:
+				x_i = [1.] + x_i
+			values.append(Precedent(x_i, sample_i[i_y]))
 		return super().__new__(cls, values)
 
 
-class SG:
+class SG_simplified:
 	"""
 	Stochastic Gradient
+
+	uses vector multiplication
 	"""
 	sample: Sample
 	rate_learning: float
@@ -104,10 +107,10 @@ class SG:
 
 	def __init__(self, sample, learning_rate, forgetting_rate):
 		"""
-		:param sample: [[x ... , y]]
+		:param Sample sample: [[x ... , y]]
 		:param forgetting_rate: functional smoothing
 		"""
-		self.sample = Sample(sample)
+		self.sample = sample
 		self.rate_learning = learning_rate
 		self.rate_forgetting = forgetting_rate
 
@@ -118,9 +121,6 @@ class SG:
 
 	def _init_weights(self):
 		"""
-		weights start from 1 in order to accommodate
-		weights[0] = decision threshold
-
 		:rtype: list[float]
 		"""
 		precedent_length = len(self.sample[0].x)
@@ -143,7 +143,7 @@ class SG:
 		"""
 		result = 0
 		for i in range(len(self.sample)):
-			result += self.precedent_loss(i)
+			result += self.loss_precedent(i)
 		return result
 
 	def _get_precedent_pos(self):
@@ -159,28 +159,102 @@ class SG:
 		q_prev = numpy.array(quality_previous)
 		w = numpy.array(self.weights)
 		w_prev = numpy.array(weights_previous)
-		if (iteration > 1) and (1.e+3 > numpy.sum(q_prev - q)) and (0 == numpy.sum(w_prev - w)) \
-				or (iteration > (2 * len(self.sample))):
+
+		def q_is_stable():
+			return 1.e+3 > numpy.sum(q_prev - q)
+
+		def weights_stopped_changing():
+			return 0 == numpy.sum(w_prev - w)
+
+		def too_much_iterations():
+			return iteration > (1.5 * len(self.sample))
+
+		if (iteration > 1) and q_is_stable() and weights_stopped_changing() or too_much_iterations():
 			print('Reason for stop:')
-			print('\tQuality is stable: 1.e+3 > numpy.sum(q_prev - q)', 1.e+3 > numpy.sum(q_prev - q))
-			print('\tweights stopped changing: 0 == numpy.sum(w_prev - w)', 0 == numpy.sum(w_prev - w))
-			print('\tOR iteration > (2 * len(self.sample))', iteration > (2 * len(self.sample)))
+			print('\tquality is stable:', q_is_stable())
+			print('\tweights stopped changing:', weights_stopped_changing())
+			print('\tOR too much iterations:', too_much_iterations(), iteration)
 			result = True
 		return result
 
 	def calculate(self):
+		def is_overflow():
+			return numpy.isinf(self.weights).any() or numpy.isnan(self.weights).any() \
+				   or numpy.isinf(self.quality[0]).any() or numpy.isnan(self.quality[0]).any()
+
 		i = 1
 		while not self.is_stop_calculating(self.quality.prev(),
 										   self.weights.prev(), i):
 			pos = self._get_precedent_pos()
-			self.errors[pos] = self.precedent_loss(pos)
+			# unclear: loss(<w, x[i]> y[i]) -- one or two args?
+			self.errors[pos] = self.loss_precedent(pos)
 			self.weights.set(self.gradient_descent(pos))
 			# (1-rf)*Q+rf*loss
 			self.quality[0] = numpy.dot(1 - self.rate_forgetting, self.quality[0]) \
 							  + self.rate_forgetting * self.errors[pos]
+
+			if is_overflow():
+				# raise ArithmeticError('Weights overflow at iteration: ' + str(i))
+				print('***ERROR***', 'Overflow at iteration:', i)
+				i = 10000
+
 			i += 1
 
-		return i
+		return [i, self.quality.prev(), self.weights.prev()]
+
+	def algorithm(self, index):
+		"""
+		:return: <w, x>
+		:rtype: float
+		"""
+		return numpy.dot(self.weights, self.sample[index].x)
+
+	def loss(self, y1, y2=1):
+		"""
+		Loss function
+
+		error
+
+		bicubic
+
+		:return: (y1 - y2) ** 2
+		"""
+		return (y1 - y2) ** 2
+
+	def loss_precedent(self, index):
+		"""
+		Calculates loss of algorithm for precedent on index
+
+		:return: loss(alg(w, x[i]), y[i])
+		"""
+		return self.loss(self.algorithm(index), self.sample[index].y)
+
+	def loss_diff(self, y1, y2=1):
+		"""
+		loss derivative by y1
+
+		:return: 2 * (y1 - y2)
+		"""
+		return 2 * (y1 - y2)
+
+	def gradient_descent(self, index):
+		# w - learning_rate * {'}loss * x[i] * y[i]
+		loss = self.loss_diff(self.algorithm(index), self.sample[index].y)
+		lrl = self.rate_learning * loss
+		lrlx = [lrl * x_i for x_i in self.sample[index].x]
+		lrlxy = lrlx * self.sample[index].y
+		return numpy.array(self.weights) - lrlxy
+
+
+class SG(SG_simplified):
+	"""
+	Stochastic Gradient
+
+	weights[0] is reserved for decision threshold
+
+	x should start from 1 in order to multiply to weights
+	x[0] = artificial constant attribute
+	"""
 
 	def activate(self, z):
 		"""
@@ -201,9 +275,9 @@ class SG:
 			result = z
 		return result
 
-	def diff_activate(self, z):
+	def activate_diff(self, z):
 		"""
-		activate derivative
+		activation derivative
 
 		:return: 1
 		"""
@@ -221,46 +295,29 @@ class SG:
 			result += self.weights[j] * self.sample[index].x[j] - self.weights[0]
 		return self.activate(result)
 
-	def precedent_loss(self, index):
-		""" Calculates loss for precedent on index """
-		return self.loss(self.algorithm(index), self.sample[index].y)
-
-	def loss(self, y1, y2):
-		"""
-		Loss function
-
-		error
-
-		bicubic
-
-		:return: (y1 - y2) ** 2
-		"""
-		return (y1 - y2) ** 2
-
-	def diff_loss(self, y1, y2):
-		"""
-		loss derivative by y1
-
-		:return: 2 * (y1- y2)
-		"""
-		return 2 * (y1 - y2)
-
 	def gradient_descent(self, index):
+		# diff
 		# w - learning_rate * {'a}loss * {'}activate(<w, x[i]>) * x[i]
-		g1 = self.diff_loss(self.algorithm(index), self.sample[index].y)
-		g2 = self.rate_learning * g1
-		g3 = g1 * g2
-		g4 = g3 * self.diff_activate(numpy.dot(self.weights, self.sample[index].x))
-		g5 = [g4 * x_i for x_i in self.sample[index].x]
-		g6 = numpy.array(self.weights) - g5
-		return g6  # diff
+		loss = self.loss_diff(self.algorithm(index), self.sample[index].y)
+		act = self.activate_diff(numpy.dot(self.weights, self.sample[index].x))
+		lrla = self.rate_learning * loss * act
+		lrlax = [lrla * x_i for x_i in self.sample[index].x]
+		result = numpy.array(self.weights) - lrlax
+		return result
 
 
-sample_test = [
+test_sample = [
 	[1., 2., 0.5],
 	[3., 4., 0.5],
 	[5., 6., 0.5]
 ]
+
+learning_rate = 0.05
+
+
+def forgetting_rate(sample):
+	return 1 / len(sample)
+
 
 import urllib.request as urlr
 
@@ -268,9 +325,16 @@ url1 = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/w
 raw_data_1 = urlr.urlopen(url1)
 dataset1 = numpy.loadtxt(raw_data_1, delimiter=";", skiprows=1)
 
-sg1 = SG(dataset1, 0.05, 1 / len(dataset1))
+sg1 = SG(Sample(dataset1), learning_rate, forgetting_rate(dataset1))
 print('Calculate:', sg1.calculate())
 print('weights:', sg1.weights)
 print('w.log:', sg1.weights.log)
 print('quality:', sg1.quality)
 print('q.log:', sg1.quality.log)
+
+sg2 = SG(Sample(dataset1, add_const_attr=True), learning_rate, forgetting_rate(dataset1))
+print('Calculate:', sg2.calculate())
+print('weights:', sg2.weights)
+print('w.log:', sg2.weights.log)
+print('quality:', sg2.quality)
+print('q.log:', sg2.quality.log)
